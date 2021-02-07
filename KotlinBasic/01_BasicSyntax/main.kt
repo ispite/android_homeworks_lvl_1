f9a40f0232fba81aabf2cf5cbe4ce1fbfb090ce6
Solovyov Denis <infinityx93@gmail.com>
2021-02-07 16:58:27 +0300

    Homework 01 BasicSyntax

--- KotlinBasic/01_BasicSyntax/main.kt
+++ KotlinBasic/01_BasicSyntax/main.kt
@@ -1,4 +1,14 @@
 
 fun main(){
-    print("Hello World!")
+    val firstName = "Denis"
+    val lastName = "Solovyov"
+    var height = 187
+    val weight = 33.5f
+    var isChild = height < 150 && weight <40
+    var info = "My name is ${firstName+' '+lastName}. My height is $height cm. My weigh is $weight kg. I'm ${if (isChild) "a child" else "an adult"}"
+    println(info)
+    height = 139
+    isChild = height < 150 && weight <40
+    info = "My name is ${firstName+' '+lastName}. My height is $height cm. My weigh is $weight kg. I'm ${if (isChild) "a child" else "an adult"}"
+    println(info)
 }
