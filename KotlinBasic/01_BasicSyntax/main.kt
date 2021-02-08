116766deb2543dd260ff94c00d6704ee264104de
Denis Solovyov <infinityx93@gmail.com>
2021-02-08 22:53:03 +0300

    correct code style + line break

--- KotlinBasic/01_BasicSyntax/main.kt
+++ KotlinBasic/01_BasicSyntax/main.kt
@@ -1,14 +1,15 @@
-
-fun main(){
+fun main() {
     val firstName = "Denis"
     val lastName = "Solovyov"
     var height = 187
     val weight = 33.5f
-    var isChild = height < 150 && weight <40
-    var info = "My name is ${firstName+' '+lastName}. My height is $height cm. My weigh is $weight kg. I'm ${if (isChild) "a child" else "an adult"}"
+    var isChild = height < 150 && weight < 40
+    var info = "My name is ${firstName + ' ' + lastName}. My height is $height cm. My weigh is " +
+            "$weight kg. I'm ${if (isChild) "a child" else "an adult"}"
     println(info)
     height = 139
-    isChild = height < 150 && weight <40
-    info = "My name is ${firstName+' '+lastName}. My height is $height cm. My weigh is $weight kg. I'm ${if (isChild) "a child" else "an adult"}"
+    isChild = height < 150 && weight < 40
+    info = "My name is ${firstName + ' ' + lastName}. My height is $height cm. My weigh is " +
+            "$weight kg. I'm ${if (isChild) "a child" else "an adult"}"
     println(info)
 }
