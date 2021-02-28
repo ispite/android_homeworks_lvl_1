b4cc97ae02a47beab175b7c7b5f6e7a4c340f417
Denis Solovyov <infinityx93@gmail.com>
2021-02-28 17:33:01 +0300

    Homework 4 syntactic sugar

--- KotlinBasic/04_Collections/collections.kt
+++ KotlinBasic/04_Collections/collections.kt
@@ -11,68 +11,49 @@ fun main() {
     loopForPhoneList(n)/*Другой способ собрать список телефонов пользователей*/
 }
 
-fun loopForPhoneListString(n: Int): List<String> {
-    val phoneList: MutableList<String> = mutableListOf()
+fun loopForPhoneListString(n: Int) = mutableListOf<String>().apply {
     var i = 0
-    var temp: String?
     while (i < n) {
         print("Введите номер телефона № ${i + 1} ")
-        temp = readLine()
-        if (temp != null && temp.removePrefix("+").all() { it.isDigit() }) {
-            phoneList.add(temp)
-        } else continue
-        i++
+        val temp = readLine()
+        temp?.let {
+            if (temp.removePrefix("+").all() { it.isDigit() }) {
+                add(temp)
+                i++
+            }
+        }
     }
-    return phoneList
 }
 
 //Другой способ собрать список телефонов пользователей
-fun loopForPhoneList(n: Int): List<String> {
-    val list: MutableList<Long> = mutableListOf()
+fun loopForPhoneList(n: Int) = mutableListOf<Long>().apply {
     val listString: MutableList<String> = mutableListOf()
     var i = 0
     while (i < n) {
         print("Введите номер телефона № ${i + 1} ")
-        list.add(readLine()?.toLongOrNull() ?: continue)
-        listString.add(if (list[i] > 0) {
-            '+' + list[i].toString()
-        } else list[i].toString())
+        this.add(readLine()?.toLongOrNull() ?: continue)
+        listString.add(if (this[i] > 0) {
+            '+' + this[i].toString()
+        } else this[i].toString())
         i++
     }
-    return listString
 }
 
-fun listSpecified(list: List<String>, key: String = "+7"): List<String> {
-    return list.filter { it.startsWith(key) }
-}
+fun listSpecified(list: List<String>, key: String = "+7") = list.filter { it.startsWith(key) }
 
-fun sizeOfSet(list: List<String>): Int {
-    return list.toSet().size
-}
+fun sizeOfSet(list: List<String>) = list.toSet().size
 
-fun sumLengthPhones(list: List<String>): Int {
-    return list.sumBy { it.length }
-}
+fun sumLengthPhones(list: List<String>) = list.sumBy { it.length }
 
-fun createPhoneMap(list: List<String>): Map<String, String> {
-    val map = mutableMapOf<String, String>()
-    for (i in list) {
+fun createPhoneMap(list: List<String>) = mutableMapOf<String, String>().apply {
+    list.forEach { i ->
         print("Введите имя человека с номером телефона $i: ")
-        readLine()?.let { map.put(i, it) }
+        readLine()?.let { put(i, it) }
     }
-    return map
 }
 
 //Печать результата с помощью entry
-fun printResult(map: Map<String, String>) {
-    for (i in map) {
-        println("Человек: ${map[i.key]}. Номер телефона: ${i.key}")
-    }
-}
+fun printResult(map: Map<String, String>) = map.forEach { i -> println("Человек: ${map[i.key]}. Номер телефона: ${i.key}") }
 
 //Печать результата с помощью пары ключ-значение
-fun printResultPair(map: Map<String, String>) {
-    for ((k, v) in map) {
-        println("Человек: $v. Номер телефона: $k")
-    }
-}
+fun printResultPair(map: Map<String, String>) = map.forEach { (k, v) -> println("Человек: $v. Номер телефона: $k") }
