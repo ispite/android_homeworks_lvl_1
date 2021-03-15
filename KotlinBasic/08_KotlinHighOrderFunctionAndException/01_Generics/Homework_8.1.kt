d9345362e1b1bc42b440e94b4c5499d3b7a1827d
Solovyov Denis <infinityx93@gmail.com>
2021-03-15 20:37:35 +0300

    Homework 8.1 | done with assistance from the teacher

--- KotlinBasic/08_KotlinHighOrderFunctionAndException/01_Generics/Homework_8.1.kt
+++ KotlinBasic/08_KotlinHighOrderFunctionAndException/01_Generics/Homework_8.1.kt
@@ -20,14 +20,29 @@ fun main() {
     println(queue.dequeue())
 
     val object1 = Result.Success<Int, String>(12)
-    object1.c1 = object1.returnedRandomResult()
-    object1.c2 = object1.returnedRandomResult()
-    object1.d1 = object1.returnedRandomResult()
-    object1.d2 = object1.returnedRandomResult()
-//    object1.e1 = object1.returnedRandomResult()
-//    object1.e2 = object1.returnedRandomResult()
-//    object1.f1 = object1.returnedRandomResult()
-//    object1.f2 = object1.returnedRandomResult()
+
+    var c1: Result<out Number, in String> = Result.Success(0)
+    var c2: Result<out Number, in String> = Result.Error("0")
+    var d1: Result<out Any, in String> = Result.Success(0)
+    var d2: Result<out Any, in String> = Result.Error("0")
+
+    var e1: Result<Int, CharSequence> = Result.Success(0)
+    var e2: Result<Int, CharSequence> = Result.Error("0")
+    var f1: Result<Int, Any> = Result.Success(0)
+    var f2: Result<Int, Any> = Result.Error("0")
+
+    c1 = object1.returnedRandomResult()
+    println(c1)
+    c2 = object1.returnedRandomResult()
+    println(c2)
+    d1 = object1.returnedRandomResult()
+    println(d1)
+    d2 = object1.returnedRandomResult()
+    println(d2)
+//    e1 = object1.returnedRandomResult()
+//    e2 = object1.returnedRandomResult()
+//    f1 = object1.returnedRandomResult()
+//    f2 = object1.returnedRandomResult()
 }
 
 inline fun <reified T> listOfNumbers(list: List<T>): List<T> {
--- KotlinBasic/08_KotlinHighOrderFunctionAndException/01_Generics/classes_08/Result.kt
+++ KotlinBasic/08_KotlinHighOrderFunctionAndException/01_Generics/classes_08/Result.kt
@@ -8,18 +8,8 @@ sealed class Result<T, R> {
     data class Error<T, R>(val exception: R) : Result<T, R>()
 
     fun returnedRandomResult(): Result<Int, String> {
-        val success: Result<Int,String> = Success(10)
-        val error: Result<Int,String> = Error("error")
+        val success: Result<Int, String> = Success(10)
+        val error: Result<Int, String> = Error("error")
         return if (Random.nextBoolean()) success else error
     }
-
-    var c1 : Result<out Number, in String> = Success(0)
-    var c2 : Result<out Number, in String> = Error("0")
-    var d1 : Result<out Any, in String> = Success(0)
-    var d2 : Result<out Any, in String> = Error("0")
-
-    var e1 : Result<Int, CharSequence> = Success(0)
-    var e2 : Result<Int, CharSequence> = Error("0")
-    var f1 : Result<Int, Any> = Success(0)
-    var f2 : Result<Int, Any> = Error("0")
 }
