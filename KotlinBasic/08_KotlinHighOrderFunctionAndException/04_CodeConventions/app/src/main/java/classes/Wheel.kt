a57ee2d920e2867d9a584913321323a84199d5eb
Denis Solovyov <infinityx93@gmail.com>
2021-03-27 12:13:45 +0300

    Homework 8.4 | new start

---
+++ KotlinBasic/08_KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/classes/Wheel.kt
@@ -0,0 +1,53 @@
+package classes
+
+class Wheel {
+    var pressure = 0.0
+        private set
+
+    private var oldPressure = 0.0
+
+    class TooHighPressure(message: String) : Exception(message)
+
+    class TooLowPressure(message: String) : Exception(message)
+
+    class IncorrectPressure(message: String) : Exception(message)
+
+    private fun setPressure(value: Double) {
+        oldPressure = pressure
+        pressure = value
+        if (pressure > 10) {
+            print("BOOM! ")
+        }
+        check()
+        println("Success pressure set $pressure")
+    }
+
+    fun catchPressureException(value: Double): Boolean {
+        try {
+            setPressure(value)
+            return true
+        } catch (t: Throwable) {
+            pressure = oldPressure
+            println("Set pressure message ${t.message}")
+        }
+        return false
+    }
+
+    private fun check() {
+        when (pressure) {
+            in -1 * Double.MAX_VALUE..-1 * Double.MIN_VALUE -> throw IncorrectPressure("Incorrect Pressure")
+            in 0.0..1.6 -> throw TooLowPressure("Too Low Pressure")
+            in 2.5..Double.MAX_VALUE -> throw TooHighPressure("Too High Pressure")
+        }
+    }
+
+    fun catchCheck() {
+        try {
+            check()
+        } catch (t: Throwable) {
+            println("Check pressure message ${t.message}")
+        }
+    }
+
+
+}
---
+++ KotlinBasic/08_KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/ru/skillbox/codeconventions_hw_8_4/Homework_8.4.kt
@@ -0,0 +1,12 @@
+package ru.skillbox.codeconventions_hw_8_4
+
+import classes.Wheel
+
+fun main() {
+    val wheel = Wheel()
+    wheel.catchCheck()
+    wheel.catchPressureException(-2.0)
+    wheel.catchPressureException(2.0)
+    wheel.catchPressureException(20.0)
+    wheel.catchCheck()
+}
