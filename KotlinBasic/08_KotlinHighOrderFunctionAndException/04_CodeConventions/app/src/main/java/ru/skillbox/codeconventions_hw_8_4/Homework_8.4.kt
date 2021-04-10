2ca05dbaa0eda0b2f726945f4591a58aa33804e2
Denis Solovyov <infinityx93@gmail.com>
2021-04-10 16:16:40 +0300

    Homework 8.4 | added some crlf

--- KotlinBasic/08_KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/ru/skillbox/codeconventions_hw_8_4/Homework_8.4.kt
+++ KotlinBasic/08_KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/ru/skillbox/codeconventions_hw_8_4/Homework_8.4.kt
@@ -6,7 +6,12 @@ fun main() {
     val wheel = Wheel()
     wheel.catchCheck()
     wheel.catchPressureException(-2.0)
+    
     wheel.catchPressureException(2.0)
+
     wheel.catchPressureException(20.0)
+
     wheel.catchCheck()
+
+
 }
