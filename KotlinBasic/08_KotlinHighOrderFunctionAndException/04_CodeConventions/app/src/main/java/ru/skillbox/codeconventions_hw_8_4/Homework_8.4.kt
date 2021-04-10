23ecc85bc06f495f900894a5a5c38acd94b657f4
Denis Solovyov <infinityx93@gmail.com>
2021-04-10 16:19:18 +0300

    Homework 8.4 | added some semicolons

--- KotlinBasic/08_KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/ru/skillbox/codeconventions_hw_8_4/Homework_8.4.kt
+++ KotlinBasic/08_KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/ru/skillbox/codeconventions_hw_8_4/Homework_8.4.kt
@@ -3,15 +3,10 @@ package ru.skillbox.codeconventions_hw_8_4
 import classes.Wheel
 
 fun main() {
-    val wheel = Wheel()
-    wheel.catchCheck()
-    wheel.catchPressureException(-2.0)
-    
-    wheel.catchPressureException(2.0)
-
-    wheel.catchPressureException(20.0)
-
-    wheel.catchCheck()
-
-
+    val wheel = Wheel();
+    wheel.catchCheck();
+    wheel.catchPressureException(-2.0);
+    wheel.catchPressureException(2.0);
+    wheel.catchPressureException(20.0);
+    wheel.catchCheck();
 }
