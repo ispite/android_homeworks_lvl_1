cef4552429925c5233c922a780b05478befc3d1e
Denis Solovyov <infinityx93@gmail.com>
2021-04-24 17:50:30 +0300

    Homework 11.2 | done

--- KotlinBasic/11_ConstraintLayoutAndToolbars/02_Toolbar/app/src/main/java/ru/skillbox/constraintlayoutandtoolbars_11_2/CoordinatorActivity.kt
+++ KotlinBasic/11_ConstraintLayoutAndToolbars/02_Toolbar/app/src/main/java/ru/skillbox/constraintlayoutandtoolbars_11_2/CoordinatorActivity.kt
@@ -1,16 +1,84 @@
 package ru.skillbox.constraintlayoutandtoolbars_11_2
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import kotlinx.android.synthetic.main.activity_coordinator.*
 
-class CoordinatorActivity:AppCompatActivity() {
+class CoordinatorActivity : AppCompatActivity() {
+
+    private val users = listOf(
+            "User1",
+            "User2",
+            "User3",
+            "Unknown"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator)
+        toolbar.setTitle(R.string.app_name_activity)
+        toolbar.title = "Amazing Homework 11.2"
+        initToolbar()
+
+    }
+
+    private fun toast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initToolbar() {
         toolbar.setNavigationOnClickListener {
-            Toast.makeText(this, "sada", Toast.LENGTH_LONG).show()
+            toast("pressed")
         }
+
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_1 -> {
+                    toast("action 1 is clicked")
+                    true
+                }
+                R.id.action_2 -> {
+                    toast("action 2 is clicked")
+                    true
+                }
+                R.id.action_3 -> {
+                    toast("action 3 is clicked")
+                    true
+                }
+                else -> false
+            }
+        }
+        val searchItem = toolbar.menu.findItem(R.id.action_search)
+        searchItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                ExpandTextView.text = "search expanded"
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                ExpandTextView.text = "search collapsed"
+                return true
+            }
+        })
+
+        (searchItem.actionView as SearchView).setOnQueryTextListener(
+                object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        return true
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        users.filter { it.contains(other = newText ?: "", ignoreCase = true) }
+                                .joinToString()
+                                .let {
+                                    SearchResultTextView.text = it
+                                }
+                        return true
+                    }
+
+                })
     }
 }
