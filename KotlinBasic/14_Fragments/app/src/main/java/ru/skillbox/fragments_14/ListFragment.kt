d707b37b09cb3e1d62017d93393f21ebda30dd56
Solovyov Denis <infinityx93@gmail.com>
2021-07-19 20:23:16 +0300

    Homework 14 | try setup recycleView

--- KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/ListFragment.kt
+++ KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/ListFragment.kt
@@ -4,3 +4,7 @@ import androidx.fragment.app.Fragment
 
 class ListFragment:Fragment(R.layout.fragment_list) {
 }
+
+//http://developer.alexanderklimov.ru/android/views/recyclerview-kot.php
+//https://devcolibri.com/unit/%D1%83%D1%80%D0%BE%D0%BA-10-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0-%D1%81-recyclerview-%D0%BD%D0%B0-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D0%B5-tweetsrecyclerview-2/
+//https://overcoder.net/q/85120/%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE%D0%B9-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80-android-recyclerview
--- KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/MainFragment.kt
+++ KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/MainFragment.kt
@@ -1,7 +1,36 @@
 package ru.skillbox.fragments_14
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class MainFragment:Fragment(R.layout.fragment_main) {
+
+    private val tagFragment = "MainFragment"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        showListFragment()
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_main)
+        DebugLogger.d(tagFragment, "onCreate was called")
+
+        showListFragment()
+    }*/
+
+    private fun showListFragment() {
+        childFragmentManager.beginTransaction()
+            .add(R.id.containerFragmentMain, ListFragment())
+            .commit()
+    }
 
 }
