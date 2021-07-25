36f7f45b29a5ad30d165e7b94031e28eab2c04d1
Denis Solovyov <infinityx93@gmail.com>
2021-07-25 17:54:48 +0300

    Homework 14 | added click Listener

--- KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/CustomRecyclerAdapter.kt
+++ KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/CustomRecyclerAdapter.kt
@@ -5,17 +5,38 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_login.view.*
+import kotlinx.android.synthetic.main.recyclerview_item.view.*
 
-class CustomRecyclerAdapter(private val names: List<String>):
+class CustomRecyclerAdapter(
+    private val names: List<String>,
+    private val listener: OnItemClickListener
+    ):
     RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        var largeTextView: TextView? = null
-        var smallTextView: TextView? = null
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+    View.OnClickListener{
+        val bigText : TextView = itemView.textViewLarge
+        val smallText : TextView = itemView.textViewSmall
+        //var largeTextView: TextView? = null
+        //var smallTextView: TextView? = null
 
         init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+
+        }
+
+        /*init {
             largeTextView = itemView.findViewById(R.id.textViewLarge)
             smallTextView = itemView.findViewById(R.id.textViewSmall)
-        }
+        }*/
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -26,9 +47,14 @@ class CustomRecyclerAdapter(private val names: List<String>):
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.largeTextView?.text = names[position]
-        holder.smallTextView?.text = "кот"
+        holder.bigText.text = names[position]
+        holder.smallText.text = "кот"
     }
 
     override fun getItemCount() = names.size
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
 }
--- KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/ListFragment.kt
+++ KotlinBasic/14_Fragments/app/src/main/java/ru/skillbox/fragments_14/ListFragment.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
-class ListFragment:Fragment(R.layout.fragment_list) {
+class ListFragment:Fragment(R.layout.fragment_list), CustomRecyclerAdapter.OnItemClickListener {
 
     /*override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,6 +42,8 @@ class ListFragment:Fragment(R.layout.fragment_list) {
         return super.onCreateView(inflater, container, savedInstanceState)
     }*/
 
+    val exampleList = fillList()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -49,8 +52,15 @@ class ListFragment:Fragment(R.layout.fragment_list) {
         val rootView = inflater.inflate(R.layout.fragment_list, container, false)
         val videoRecyclerView = rootView.findViewById(R.id.recyclerView) as RecyclerView
         videoRecyclerView.layoutManager = LinearLayoutManager(activity)
-        videoRecyclerView.adapter = CustomRecyclerAdapter(fillList())
+        videoRecyclerView.adapter = CustomRecyclerAdapter(exampleList, this)
         return rootView
+    }
+
+    override fun onItemClick(position: Int) {
+        toast("Item $position clicked")
+        val clickedItem = exampleList[position]
+        //clickedItem
+        //adapter.notifyItemChanged
     }
 
     private fun fillList(): List<String> {
@@ -58,7 +68,15 @@ class ListFragment:Fragment(R.layout.fragment_list) {
         (1..30).forEach { i -> data.add("$i элемент") }
         return data
     }
+
+    private fun toast(text: String) {
+        //Toast.makeText(this, text, 123).show()
+        Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+    }
 }
+
+//https://codinginflow.com/tutorials/android/simple-recyclerview-kotlin/part-4-click-handler
+//https://www.youtube.com/watch?v=wKFJsrdiGS8
 
 //http://developer.alexanderklimov.ru/android/views/recyclerview-kot.php
 //https://devcolibri.com/unit/%D1%83%D1%80%D0%BE%D0%BA-10-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0-%D1%81-recyclerview-%D0%BD%D0%B0-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D0%B5-tweetsrecyclerview-2/
