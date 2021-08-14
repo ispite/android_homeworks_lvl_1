df1f522785848917e3978ba815c68ca4df9afba6
Denis Solovyov <infinityx93@gmail.com>
2021-08-14 18:29:10 +0300

    Homework 16 | creating a new project

---
+++ KotlinBasic/16_Lists_1/app/src/main/java/ru/skillbox/a16_lists_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.skillbox.a16_lists_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
