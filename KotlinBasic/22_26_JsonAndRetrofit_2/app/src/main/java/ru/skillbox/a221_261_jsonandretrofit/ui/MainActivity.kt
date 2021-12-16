bcab1f6e7b065e890532e02572474cfc523f99b7
Denis Solovyov <infinityx93@gmail.com>
2021-12-16 18:28:28 +0300

    Homework 22.2 (26.2) | started second Homework

---
+++ KotlinBasic/22_26_JsonAndRetrofit_2/app/src/main/java/ru/skillbox/a221_261_jsonandretrofit/ui/MainActivity.kt
@@ -0,0 +1,7 @@
+package ru.skillbox.a221_261_jsonandretrofit.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import ru.skillbox.a221_261_jsonandretrofit.R
+
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ KotlinBasic/22_26_JsonAndRetrofit_2/app/src/main/java/ru/skillbox/a221_261_jsonandretrofit/ui/current_user/CurrentUserFragment.kt
@@ -0,0 +1,7 @@
+package ru.skillbox.a221_261_jsonandretrofit.ui.current_user
+
+import androidx.fragment.app.Fragment
+
+class CurrentUserFragment: Fragment() {
+
+}
