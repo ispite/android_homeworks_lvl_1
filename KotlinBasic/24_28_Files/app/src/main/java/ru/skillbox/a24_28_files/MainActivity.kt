b6e1984a183914e4a4e37020e4a9e00ad6045116
Denis Solovyov <infinityx93@gmail.com>
2022-03-22 12:21:54 +0300

    Homework 24 (28) | starting a new project

---
+++ KotlinBasic/24_28_Files/app/src/main/java/ru/skillbox/a24_28_files/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.skillbox.a24_28_files
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
