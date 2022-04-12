b198cbee223d1ba54cade56821da658f9631bc95
Denis Solovyov <infinityx93@gmail.com>
2022-04-12 18:53:27 +0300

    Homework 25 (29) | created a test app

---
+++ KotlinBasic/25_29_ContentProviderTest/app/src/main/java/ru/skillbox/a25_29_contentprovidertest/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.skillbox.a25_29_contentprovidertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
