441b9353dc97cdbbc0b8f8883e648a0087d1fee2
Denis Solovyov <infinityx93@gmail.com>
2023-01-04 11:19:15 +0300

    Homework 32 (36) | starting a new project

---
+++ KotlinBasic/32_36_MaterialDesign/app/src/main/java/ru/skillbox/a32_36_materialdesign/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.skillbox.a32_36_materialdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
